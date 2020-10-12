605c12e23ec982422e91c1831781221a83cf8511
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2020-10-12 19:20:12 -0400

    Update main.py

--- Unit 3 Python/PyBank/main.py
+++ Unit 3 Python/PyBank/main.py
@@ -2,11 +2,17 @@ import csv
 
 with open('budget_data.csv') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    
+
+    totalrev = 0
+
     header = next(csvreader)
     row_count = sum(1 for row in csvreader)
-    print(f"Total Months: {row_count}")
-        
-
-        
     
+    for row in csvreader:
+
+        count += 1
+        revenue = float(row[1])     
+        totalrev += revenue
+
+print(f"Total Months: {row_count}")
+print(totalrev)
