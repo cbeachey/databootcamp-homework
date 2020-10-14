795815d4b96b5ab7c6ae178d419c181b79e1d8f4
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2020-10-13 23:53:27 -0400

    Update main.py

--- Unit 3 Python/PyBank/main.py
+++ Unit 3 Python/PyBank/main.py
@@ -32,8 +32,18 @@ with open('budget_data.csv') as csvfile:
         sum_profit = sum(profit_loss_change)
         avg_profit_loss = round(sum_profit/(total_months - 1), 2)
 
+    greatest_increase = max(profit_loss_change)
+    greatest_decrease = min(profit_loss_change)
+
+    best_month = profit_loss_change.index(greatest_increase)
+    worst_month = profit_loss_change.index(greatest_decrease)
+
+    best_month_final = str(months[best_month])
+    worst_month_final = str(months[worst_month])
+
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")    
 print(f"Total: ${totalrev}")
 print(f"Average Change: {avg_profit_loss}")
+print(best_month_final)
