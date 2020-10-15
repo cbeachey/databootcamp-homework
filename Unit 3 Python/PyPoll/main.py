9857af385866567417d0ba3269f7fc377599a222
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2020-10-14 23:32:15 -0400

    Update main.py

--- Unit 3 Python/PyPoll/main.py
+++ Unit 3 Python/PyPoll/main.py
@@ -1,6 +1,9 @@
 import csv
 
 votes = []
+candidate = []
+vote_count = {}
+percentage = {}
 
 total_votes = 0
 
@@ -9,10 +12,24 @@ with open('election_data.csv') as csvfile:
     header = next(csvreader)
 
     for row in csvreader:
-       total_votes += 1
+        total_votes += 1
+       # if total_votes == 10:
+        #    break
+        if row[2] not in candidate:
+            candidate.append(row[2])     
+
+        if row[2] not in vote_count:
+            vote_count[row[2]]=1
+        
+        else:
+           vote_count[row[2]] += 1
+
+            
+         
+
 
 print(f'Election Results')
 print(f'-------------------------')
 print(f'Total Votes: {total_votes}') 
 print(f'-------------------------')
-
+print(vote_count)
