564e43b723d3333d7273d16e697c5f85724704a9
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2020-11-27 23:07:18 -0500

    finished

--- Unit 8 SQL/data/query.sql
+++ Unit 8 SQL/data/query.sql
@@ -89,3 +89,8 @@ OR departments.dept_name = 'Development';
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., 
 --    how many employees share each last name.
+
+SELECT last_name
+	, count(last_name) as "Name Count" FROM employees
+	GROUP BY last_name
+ORDER BY "Name Count" DESC;
