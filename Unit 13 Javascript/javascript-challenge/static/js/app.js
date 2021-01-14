e417c6ff083a5dfde15d61cf86557c76fbd7b531
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2021-01-13 22:47:01 -0500

    Update app.js

--- Unit 13 Javascript/javascript-challenge/static/js/app.js
+++ Unit 13 Javascript/javascript-challenge/static/js/app.js
@@ -5,4 +5,26 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // check console
-console.log(data)
+console.log(data);
+
+
+//data.forEach(function(ufoSightings) {
+  //  console.log(ufoSightings);
+  //  });
+
+// append
+
+
+data.forEach(function(ufoSightings) {
+    console.log(ufoSightings);
+        var row = tbody.append("tr");
+            Object.entries(ufoSightings).forEach(function([key, value]) {
+            console.log(key, value);
+     // Append a cell to the row for each value
+    // in the weather report object
+     var cell = row.append("td");
+     cell.text(value);
+   });
+ });
+ 
+    
