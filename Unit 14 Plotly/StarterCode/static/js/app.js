d58741f9c792f45e8cdc52b3edcc6fbdc9026401
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2021-01-20 20:20:11 -0500

    Update app.js

--- Unit 14 Plotly/StarterCode/static/js/app.js
+++ Unit 14 Plotly/StarterCode/static/js/app.js
@@ -1,8 +1,12 @@
 // Read in samples.json
 function getData(sample) {
     d3.json("samples.json").then((data) => {
-        console.log(data)
+       
+        // Get values from data json object
+        var names = data.samples.names
+
+
+        console.log(names)
     })
 };
 
-getData();
