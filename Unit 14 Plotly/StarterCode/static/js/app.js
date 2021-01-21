5f7918b82653f308edba319e88f0380a6f82ebda
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2021-01-20 22:39:34 -0500

    Update app.js

--- Unit 14 Plotly/StarterCode/static/js/app.js
+++ Unit 14 Plotly/StarterCode/static/js/app.js
@@ -3,24 +3,12 @@ function getData(sample) {
 
     d3.json("samples.json").then((data) => {
        
+        var data2 = data;
+        var sampleData = data2.samples;
         // Get values from data json object
        // var wfreq = data.metadata.map(d => d.wfreq)
        // console.log(wfreq);
-
-        var sampleValues = data.metadata.sample_values;
-        var otu_ids = data.otu_ids;
-        var otuLabels = data.otu_labels;
-        
-        var trace1 = {
-            x: data.otu_ids,
-            y: data.sampleValues,
-            text: otuLabels,
-            name: "",
-            type: "bar"
-          };
-        
-          Plotly.newPlot("bar", trace1);
-          
+       console.log(sampleData);
     })
 };
 
