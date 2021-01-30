df9901157121f2e3fa8a41f6c727512e903be56f
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2021-01-30 10:12:49 -0500

    Update app.js

--- Unit 15 D3/StarterCode/assets/js/app.js
+++ Unit 15 D3/StarterCode/assets/js/app.js
@@ -11,6 +11,7 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
 var svg = d3
   .select("#scatter")
   .append("svg")
@@ -22,11 +23,12 @@ var chartGroup = svg.append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-
+// Import Data
 d3.csv("assets/data/data.csv").then(function(healthData) {
 
   console.log(healthData);
 
+  // Parse Data/Cast as numbers
   healthData.forEach(function(data) {
     data.age = +data.age;
     data.smokes = +data.smokes;
