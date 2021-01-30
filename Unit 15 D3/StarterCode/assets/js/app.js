2e2c425ed8e8b8a19bf93f3db12d21a63432e7ad
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2021-01-30 10:00:37 -0500

    Update app.js

--- Unit 15 D3/StarterCode/assets/js/app.js
+++ Unit 15 D3/StarterCode/assets/js/app.js
@@ -25,3 +25,43 @@ var chartGroup = svg.append("g")
 
 d3.csv("data.csv").then(function(healthData) {
 
+  console.log(healthData);
+
+  healthData.forEach(function(data) {
+    data.age = +data.age;
+    data.smokes = +data.smokes;
+  });
+
+
+  var xLinearScale = d3.scaleLinear()
+      .domain([20, d3.max(healthData, d => d.age)])
+      .range([0, width]);
+
+  var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(healthData, d => d.smokes)])
+      .range([height, 0]);
+
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+  
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(healthData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.age))
+    .attr("cy", d => yLinearScale(d.smokes))
+    .attr("r", "15")
+    .attr("fill", "pink")
+    .attr("opacity", ".5");
+
+
+
+
+});
