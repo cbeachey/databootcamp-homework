4a6555c4799c95dc99c982f26953d7ea51e613d6
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2021-02-21 20:47:56 -0500

    Update logic.js

--- Unit 16 Plotly/static/js/logic.js
+++ Unit 16 Plotly/static/js/logic.js
@@ -18,8 +18,9 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 // URL for Earthquake Data
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson" ;
-var plate_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
+
+// Earthquake layer
 var earthquake = new L.LayerGroup();
 
 function markerSize(magnitude) {
