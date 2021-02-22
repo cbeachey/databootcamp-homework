c5cfc81b2ab0e797203aeee9996ae67828405504
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2021-02-21 20:25:07 -0500

    Update logic.js

--- Unit 16 Plotly/static/js/logic.js
+++ Unit 16 Plotly/static/js/logic.js
@@ -1,5 +1,5 @@
 // Creating map object
-var myMap = L.map("map", {
+var myMap = L.map("mapid", {
     center: [34.0522, -118.2437],
     zoom: 8
   });
@@ -18,3 +18,36 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 // URL for Earthquake Data
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson" ;
+
+function markerSize(magnitude) {
+    return magnitude * 4;
+};
+
+
+// Grab the data with d3
+d3.json(url, function(response) {
+
+    // Create a new marker cluster group
+    var markers = L.markerClusterGroup();
+  
+    // Loop through data
+    for (var i = 0; i < response.length; i++) {
+  
+      // Set the data location property to a variable
+      var location = response[i].location;
+  
+      // Check for location property
+      if (location) {
+  
+        // Add a new marker to the cluster group and bind a pop-up
+        markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+          .bindPopup(response[i].descriptor));
+      }
+  
+    }
+  
+    // Add our marker cluster layer to the map
+    myMap.addLayer(markers);
+  
+  });
+  
